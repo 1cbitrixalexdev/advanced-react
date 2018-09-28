b377eb290284ffec40dda0e5e7e58657871683ae
Stas <1cbitrix.alex.dev@gmail.com>
2018-09-28 10:36:53 +0300

    - pre-settings

---
+++ src/components/Root.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Root extends Component {
+    render() {
+        return (
+            <div>
+                Root is here
+            </div>
+        )
+    }
+}
+
+export default Root;
